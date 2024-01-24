33dee9255d878874e90881515be8a3f35cb8ae11
Guilherme Siqueira <siqueiraguilherme920@gmail.com>
2024-01-23 23:39:03 -0300

    add list item

--- src/components/TaskListItem.tsx
+++ src/components/TaskListItem.tsx
@@ -1,7 +1,34 @@
-type Props = {};
+import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
-const TaskListItem = (props: Props) => {
-  return <div>TaskListItem</div>;
+type Props = {
+  title: string;
+  dificulty: number | null;
+};
+
+const TaskListItem = ({ title, dificulty }: Props) => {
+  return (
+    <li className="bg-purple-300 rounded flex justify-between p-2">
+      <div>
+        <h3 className="font-semibold text-md">{title}</h3>
+
+        <p className="text-sm">
+          <span className="font-semibold">Dificuldade: </span>
+          {dificulty}
+        </p>
+      </div>
+
+      <div className="flex gap-1">
+        <button>
+          <FaEdit />
+        </button>
+
+        <button>
+          <FaTrashAlt />
+        </button>
+      </div>
+    </li>
+  );
 };
 
 export default TaskListItem;
--- src/components/Tasklist.tsx
+++ src/components/Tasklist.tsx
@@ -1,11 +1,27 @@
 import { getTasks } from "@/getters/getTasks";
 
+import TaskListItem from "@/components/TaskListItem";
+
 const TaskList = () => {
   const tasks = getTasks();
 
   return (
     <div>
-      {tasks.length > 0 ? <p>tem tarefas</p> : <p>você não possuí tarefas</p>}
+      {tasks.length > 0 ? (
+        <ul className="container flex flex-col gap-2">
+          {tasks.map((task) => (
+            <TaskListItem
+              key={task.id}
+              title={task.title}
+              dificulty={task.dificulty}
+            />
+          ))}
+        </ul>
+      ) : (
+        <p className="text-gray-400 text-center text-sm">
+          Você não possui tarefas! :)
+        </p>
+      )}
     </div>
   );
 };
